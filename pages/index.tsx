24409fdf83d8ed67048ff5a78e85e86d78b28620
Rakesh <rakesh.jmi90@gmail.com>
2023-11-28 21:49:08 +0530

    added nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
+import NavBar from "@/components/navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-xl underline">Home Page</h1>
-      </div>
-    </main>
+    <>
+      <NavBar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-2 mt-4">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <h1>Home Page</h1>
+        </div>
+      </main>
+    </>
   );
 }
